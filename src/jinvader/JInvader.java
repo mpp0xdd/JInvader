f32013ee4c9cb6139e600b9e4f61a83d2a4697ec
mpp <mpp0xdd@gmail.com>
2024-06-18 21:58:24 +0900

    WIP

--- src/jinvader/JInvader.java
+++ src/jinvader/JInvader.java
@@ -1,12 +1,18 @@
 package jinvader;
 
+import jglib.base.Game;
 import jglib.component.GameWindow;
 import jinvader.component.DefaultSpace;
 import jinvader.screen.MainScreen;
 
-public class JInvader {
+public class JInvader extends Game {
 
   public static void main(String[] args) {
+    launch(JInvader.class);
+  }
+
+  @Override
+  protected void start() {
     GameWindow window = new GameWindow("JInvader");
     MainScreen screen = new MainScreen(new DefaultSpace());
 
